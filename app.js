3ca253e004fb1686418e602bd7d891ce70f37582
orcunTasdemir <otasdemir@vassar.edu>
2022-05-07 19:26:46 -0400

    added heroku config

--- app.js
+++ app.js
@@ -17,7 +17,10 @@ const reviewRouter = require('./routes/reviewRoutes');
 const bookingRouter = require('./routes/bookingRoutes');
 const viewRouter = require('./routes/viewRoutes');
 
+// start express app
 const app = express();
+
+app.enable('trust proxy');
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
